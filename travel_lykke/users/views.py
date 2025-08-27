83a261b3d57c61228e78111da9f9da9e16e560fc
roy-abir05 <royabirdhn@gmail.com>
2025-08-27 19:43:34 +0530

    feat: add user registration

--- travel_lykke/users/views.py
+++ travel_lykke/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    print("Register view called", request.method)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, f'Account created for {user.username}! You can now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Please correct the error below\n{}'.format(form.errors))
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
