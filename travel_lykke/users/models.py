3a54b1941472f698176ceeed6d733c4c047dfb6b
roy-abir05 <royabirdhn@gmail.com>
2025-08-27 19:02:58 +0530

    feat: add Profile model

--- travel_lykke/users/models.py
+++ travel_lykke/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to="profile_images/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+    
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
