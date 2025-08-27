f6ef6a8bc7e6bb576454c7a6814a126e045c8a12
roy-abir05 <royabirdhn@gmail.com>
2025-08-28 00:27:53 +0530

    feat: add views for login, register and profile

--- travel_lykke/users/views.py
+++ travel_lykke/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -16,3 +17,7 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    return render(request, "users/profile.html")
