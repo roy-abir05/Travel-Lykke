81636d6301ec551d6aa4a09f8f73a16c308905cb
roy-abir05 <royabirdhn@gmail.com>
2025-08-27 12:07:13 +0530

    fix: change name of travels to travel_options

--- travel_lykke/travel_lykke/settings.py
+++ travel_lykke/travel_lykke/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'users',
-    'travels',
+    'travels_options',
     'bookings',
 ]
 
--- travel_lykke/travel_options/apps.py
+++ travel_lykke/travel_options/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TravelsConfig(AppConfig):
+class TravelOptionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'travels'
+    name = 'travel_options'
