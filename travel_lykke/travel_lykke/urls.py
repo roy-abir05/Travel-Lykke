007cf536191428c5742520c1289d3aa5f91e6f78
roy-abir05 <royabirdhn@gmail.com>
2025-08-28 23:14:23 +0530

    feat: add about us page

--- travel_lykke/travel_lykke/urls.py
+++ travel_lykke/travel_lykke/urls.py
@@ -22,6 +22,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about_us/', views.about_us, name='about_us'),
     path('admin/', admin.site.urls, name='admin'),
     path('users/', include('users.urls'), name='users'),
     path('travel_options/', include('travel_options.urls'), name='travel_options'),
