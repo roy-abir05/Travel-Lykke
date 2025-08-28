35da3f55a832bb0387e39394f4b2b5bffefc03ef
roy-abir05 <royabirdhn@gmail.com>
2025-08-28 11:47:20 +0530

    feat: travel_options view

--- travel_lykke/travel_options/urls.py
+++ travel_lykke/travel_options/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.travel_options, name='travel_options'),
+    path('<int:pk>/', views.travel_option_details, name='travel_option_details'),
+]
--- travel_lykke/travel_options/views.py
+++ travel_lykke/travel_options/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import TravelOption
 
 # Create your views here.
+
+def travel_options(request):
+    options = TravelOption.objects.all()
+    return render(request, 'travel_options/travel_options.html', {"options": options})
+
+def travel_option_details(request, pk):
+    option = get_object_or_404(TravelOption, pk=pk)
+    return render(request, "travel_options/details.html", {"option": option})
