e5f5d80700363260eab175fe7514d103b7c898df
roy-abir05 <royabirdhn@gmail.com>
2025-08-28 12:19:03 +0530

    feat: add bookings views

--- travel_lykke/bookings/urls.py
+++ travel_lykke/bookings/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('<int:option_id>/book/', views.booking_form, name="booking_form"),
+    path("my-bookings/", views.my_bookings, name="my_bookings"),
+]
--- travel_lykke/bookings/views.py
+++ travel_lykke/bookings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Booking
 
 # Create your views here.
+@login_required
+def booking_form(request, option_id):
+    # later you can fetch the travel option by ID and price
+    # for now, just hardcode the price
+    context = {"price": 258}
+    if request.method == "POST":
+        # process form submission here
+        # e.g. save booking to DB
+        return redirect("my_bookings")
+    return render(request, "bookings/booking_form.html", context)
+
+@login_required
+def my_bookings(request):
+    bookings = Booking.objects.filter(user=request.user)
+    return render(request, "bookings/my_bookings.html", {"bookings": bookings})
