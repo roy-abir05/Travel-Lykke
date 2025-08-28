895c8ad0fe8d986c2068765b12c66b2187835b62
roy-abir05 <royabirdhn@gmail.com>
2025-08-28 19:33:27 +0530

    fix: travel option price display

--- travel_lykke/travel_options/views.py
+++ travel_lykke/travel_options/views.py
@@ -10,4 +10,7 @@ def travel_options(request):
 
 def travel_option_details(request, pk):
     option = get_object_or_404(TravelOption, pk=pk)
+    tax = 10
+    setattr(option, "taxes", tax)
+    setattr(option, "base_fare", option.price - tax)
     return render(request, "travel_options/details.html", {"option": option})
